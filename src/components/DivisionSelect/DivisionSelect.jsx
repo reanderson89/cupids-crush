2f6746ffc3c8c20e6e88dc2beb187d4a0ae8c011
robAnderson <reanderson89@gmail.com>
2023-01-27 16:12:36 -0800

    added labels

--- src/components/DivisionSelect/DivisionSelect.jsx
+++ src/components/DivisionSelect/DivisionSelect.jsx
@@ -3,7 +3,7 @@ const DivisionSelect = ({ divisions }) => {
   return (
     <>
     
-    <label htmlFor="divisionSelect"></label>
+    <label htmlFor="divisionSelect">Divisions: </label>
     <select name="divisionSelect" id="divisionSelect">
       <option value="0">All</option>
         {divisions.map((division) => <option key={division.id} value={division.id}>{division.description}</option>)}
--- src/components/EventSelect/EventSelect.jsx
+++ src/components/EventSelect/EventSelect.jsx
@@ -2,7 +2,7 @@ const EventSelect = ({ events }) => {
 
   return (
     <>
-    <label htmlFor="eventSelect"></label>
+    <label htmlFor="eventSelect">Events: </label>
     <select name="eventSelect" id="eventSelect">
       <option value="0">Overall</option>
         {events.map((event, i) => <option key={event.id} value={event.id*0+i+1}>{event.description} (WOD-{i+1})</option>)}
